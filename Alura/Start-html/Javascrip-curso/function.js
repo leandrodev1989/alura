0972299d425e6334b7928a306d8e66c066fa00a4
leandrodev1989 <leandrojose125327@gmail.com>
2022-08-08 13:49:31 -0300

    Start alura

---
+++ Alura/Start-html/Javascrip-curso/function.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+function creatPhrases(){
+    console.log("Estudar é Bom");
+    console.log("Paciência e Persistência");
+    console.log("Revisão é a mãe do aprendizado");
+}
+
+creatPhrases()
+
+console.log("Fim da Função");
